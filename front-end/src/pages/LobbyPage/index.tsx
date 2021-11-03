84ec01ace400470062b71a4446eca98f062f17d2
24to26 <ckdwhzjarhk@korea.ac.kr>
2021-11-03 18:14:54 +0900

    💄 : Appbar 레이아웃 컴포넌트 작성

--- front-end/src/pages/LobbyPage/index.tsx
+++ front-end/src/pages/LobbyPage/index.tsx
@@ -1,4 +1,6 @@
 import { useHistory } from 'react-router-dom';
+import AppbarLayout from '../../layout/AppbarLayout';
+import './style.scss';
 
 type locationState = { user: { login: string }; name?: string };
 
@@ -7,11 +9,13 @@ function LobbyPage() {
     location: { state },
   } = useHistory<locationState>();
   return (
-    <div className="full__page--root">
-      <div>Lobby</div>
-      <div>WELCOME</div>
-      {/*<div>{state.user ? JSON.stringify(state.user.login) : state.name}</div>*/}
-    </div>
+    <AppbarLayout>
+      <div className="lobby__page--root">
+        <div>Lobby</div>
+        <div>WELCOME</div>
+        {/*<div>{state.user ? JSON.stringify(state.user.login) : state.name}</div>*/}
+      </div>
+    </AppbarLayout>
   );
 }
 
