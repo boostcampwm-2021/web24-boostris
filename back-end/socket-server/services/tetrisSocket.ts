b549b83ab82fec007e4b4b9ada12284e69de0666
jyh <jeonyh0521@gmail.com>
2021-11-10 14:53:28 +0900

    ✨ :  테트리스 멀티 게임 소켓 추가
    - 플레이어 방 입장 정보 전송
    - 방 입장 시 이미 입장해있던 다른 유저 정보 전송
    - 게임 시작 정보  전송
    - 블록이 떨어지는 정보 전송
    - 보드 정보 전송
    - 공격 정보 전송
    - 공격 상대 결정 구현
    - 게임 오버 전송
    
    Close #74
    Close #81

--- back-end/socket-server/services/tetrisSocket.ts
+++ back-end/socket-server/services/tetrisSocket.ts
@@ -6,6 +6,8 @@ export const initTetrisSocket = (io) => {
       players: []    
     },
   };
+  let gameOverPlayer = 0;
+  let garbageBlockCnt = [];
 
   io.on('connection', (socket) => {
     console.log(`socket ${socket.id} connect`);
@@ -31,34 +33,63 @@ export const initTetrisSocket = (io) => {
   
   socket.broadcast.to(room).emit('enter new player', socket.id);
   
-    socket.on('get other players info', res => {
-      const otherPlayer = rooms[room].players.filter(player => player != socket.id);
-      res(otherPlayer);
-    });
+  socket.on('get other players info', res => {
+    const otherPlayer = rooms[room].players.filter(player => player != socket.id);
+    res(otherPlayer);
+  });
+
+  socket.on('game start', () => { // 다른 누군가 게임 시작을 눌렀다면 다른 사용자들에게 알림
+    io.to(room).emit('game started');
+    
+    garbageBlockCnt = [];
+    rooms[room].players.forEach(player => garbageBlockCnt.push({id: player, garbageCnt: 0}));
+
+    console.log('게임 시작');
+  });
+
+  socket.on('drop block', (board, block) => { // 내 떨어지는 블록을 다른 사람에게 전송한다.
+    socket.broadcast.to(room).emit(`other player's drop block`, socket.id, board, block);
+  })
+
+  socket.on('attack other player', garbage => {
+    garbageBlockCnt.sort((a, b) => a.garbageCnt - b.garbageCnt);
+
+    let idx = 0;
+
+    if(garbageBlockCnt[idx].id === socket.id) { // 블록 수가 가장 작은 사람이 자기 자신이라면 다음 사람에게
+      idx++;
+    }
+
+    io.to(garbageBlockCnt[idx].id).emit('attacked', garbage);
+    garbageBlockCnt[idx].garbageCnt += garbage;
+  });
+
+  socket.on('game over', () => { // 누군가 게임 오버 시
+    gameOverPlayer++;
+
+    if(gameOverPlayer === rooms[room].players.length - 1) {
+      io.to(room).emit('every player game over'); // 게임 오버 메시지를 전체 전송(전체 전송은 io로)
+      gameOverPlayer = 0;
+    }
+  }); 
   
-    socket.on('drop block', block => { // 내 떨어지는 블록을 다른 사람에게 전송한다.
-      socket.broadcast.to(room).emit(`other player's drop block`, socket.id, block);
-    })
-  
-    socket.on('disconnect', () => {
-      console.log(`disconnect ${socket.id}`);
-  
-      if(rooms[online[socket.id].room].players != undefined)
-        rooms[online[socket.id].room].players = rooms[online[socket.id].room].players.filter(player => player != socket.id);
-      
-      
-      // if(rooms[online[socket.id].room].players.length == 0) { // 모두가 방을 비우면 해당 방 삭제
-      //   delete rooms[online[socket.id].room];
-      // }
-      
-      delete online[socket.id]; // 일단 온라인에서 유저도 삭제
-      
-      console.log(online);
-      console.log(rooms);
-    });
-  
-    socket.on('canvas', (data) => {
-      console.log(data);
-    });
+  socket.on('disconnect', () => {
+    console.log(`disconnect ${socket.id}`);
+
+    socket.broadcast.to(room).emit('disconnect player', socket.id); // 게임 종료 시 다른 사람들한테 전달
+
+    if(rooms[online[socket.id].room].players != undefined)
+      rooms[online[socket.id].room].players = rooms[online[socket.id].room].players.filter(player => player != socket.id);
+    
+    
+    // if(rooms[online[socket.id].room].players.length == 0) { // 모두가 방을 비우면 해당 방 삭제
+    //   delete rooms[online[socket.id].room];
+    // }
+    
+    delete online[socket.id]; // 일단 온라인에서 유저도 삭제
+    
+    console.log(online);
+    console.log(rooms);
+  });
   });  
 }
