7dc1c9f8c2df781da0df9a2e475c2ad2d060edec
24to26 <ckdwhzjarhk@korea.ac.kr>
2021-11-11 10:38:52 +0900

    🔨 : 클라이언트 라우터 리팩토링

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 
-import './App.scss';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import OauthCallbackRouter from './routes/OauthCallbackRouter';
@@ -10,6 +9,12 @@ import { useAppDispatch } from './app/hooks';
 import { checkAuth } from './features/user/userSlice';
 import useAuth from './hooks/use-auth';
 import WithSocketPage from './pages/WithSocketPage';
+import RequireAuth from './routes/RequireAuth';
+import LobbyPage from './pages/LobbyPage';
+import Tetris from './components/Tetris';
+import GamePage from './pages/GamePage';
+import RankPage from './pages/RankPage';
+import './App.scss';
 
 function App() {
   let { auth } = useAuth();
@@ -29,7 +34,33 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/oauth/*" element={<OauthCallbackRouter />} />
-        <Route path="/*" element={<WithSocketPage />} />
+        <Route path="/*" element={<WithSocketPage />}>
+          <Route
+            path=""
+            element={
+              <RequireAuth>
+                <LobbyPage />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="tetris"
+            element={
+              <RequireAuth>
+                <Tetris />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="game/:gameID"
+            element={
+              <RequireAuth>
+                <GamePage />
+              </RequireAuth>
+            }
+          />
+          <Route path="rank" element={<RankPage />} />
+        </Route>
       </Routes>
     </div>
   );
--- front-end/src/pages/WithSocketPage/index.tsx
+++ front-end/src/pages/WithSocketPage/index.tsx
@@ -1,41 +1,10 @@
-import { Route, Routes } from 'react-router-dom';
-import Tetris from '../../components/Tetris';
+import { Outlet } from 'react-router-dom';
 import SocketProvider from '../../context/SocketContext';
-import RequireAuth from '../../routes/RequireAuth';
-import GamePage from '../GamePage';
-import LobbyPage from '../LobbyPage';
-import RankPage from '../RankPage';
 
 function WithSocketPage() {
   return (
     <SocketProvider>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <RequireAuth>
-              <LobbyPage />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/tetris"
-          element={
-            <RequireAuth>
-              <Tetris />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/game/:gameID"
-          element={
-            <RequireAuth>
-              <GamePage />
-            </RequireAuth>
-          }
-        />
-        <Route path="/rank" element={<RankPage />} />
-      </Routes>
+      <Outlet />
     </SocketProvider>
   );
 }
