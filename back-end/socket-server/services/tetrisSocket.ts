e34cc2f00a4d5580d55b24684a67168911fa3375
jyh <jeonyh0521@gmail.com>
2021-11-11 17:31:29 +0900

    🔨 : 주석 제거

--- back-end/socket-server/services/tetrisSocket.ts
+++ back-end/socket-server/services/tetrisSocket.ts
@@ -17,45 +17,24 @@ export const initTetrisSocket = (io: Namespace) => {
   const rooms = {};
 
   io.on('connection', (socket: tetrisSocket) => {
-    console.log(`socket ${socket.id} connect`);
-
-    // socket.join(room);
-    // online[socket.id] = { room: room };
-    // rooms[room].players.push(socket.id);
-
     socket.on('join room', (roomId) => {
-      // 방 ID 를 고유하게 만들어서 join할 예정
-      // online[socket.id] = {room: roomId};
       if (!rooms.hasOwnProperty(roomId)) {
         rooms[roomId] = JSON.parse(JSON.stringify({ ...initialState }));
       }
       socket.join(roomId);
       socket.roomId = roomId;
-
-      //socket.broadcast.to(roomId).emit('join new player', socket.id);
-      //socket.emit('get other players info', rooms[roomId].players); // 누군가 방에 들어오면 자신을 제외한 모든 사람들 전송
-
-      // rooms[roomId].players.push(socket.id);
-
-      //if(!rooms[roomId].players) return;
-      // console.log(online);
-      // console.log(rooms);
       socket.broadcast.to(roomId).emit('enter new player', socket.id);
     });
 
     socket.on('get other players info', (res) => {
-      // const [roomID] = [...io.adapter.sids.get(socket.id)].filter((r) => r !== socket.id);
       const otherPlayer = [...io.adapter.rooms.get(socket.roomId)].filter((p) => p !== socket.id);
-      //  rooms[room].players.filter((player) => player != socket.id);
       res(otherPlayer);
     });
 
     socket.on('game start', () => {
       // 다른 누군가 게임 시작을 눌렀다면 다른 사용자들에게 알림
       io.to(socket.roomId).emit('game started');
-
       rooms[socket.roomId].garbageBlockCnt = []; // 공격 전달을 위한 배열 초기화
-
       [...io.adapter.rooms.get(socket.roomId)].forEach((player) =>
         rooms[socket.roomId].garbageBlockCnt.push({ id: player, garbageCnt: 0 })
       );
@@ -104,23 +83,15 @@ export const initTetrisSocket = (io: Namespace) => {
       rooms[socket.roomId].gameOverPlayer++;
 
       if (rooms[socket.roomId].gameOverPlayer === io.adapter.rooms.get(socket.roomId).size - 1) {
-        io.to(socket.roomId).emit('every player game over'); // 게임 오버 메시지를 전체 전송(전체 전송은 io로)
+        // 게임 오버 메시지를 전체 전송(전체 전송은 io로)
+        io.to(socket.roomId).emit('every player game over');
         rooms[socket.roomId].gameOverPlayer = 0;
       }
     });
 
     socket.on('disconnect', () => {
-      socket.broadcast.to(socket.roomId).emit('disconnect player', socket.id); // 게임 종료 시 다른 사람들한테 전달
-      // if (rooms[online[socket.id].room].players != undefined)
-      //   rooms[online[socket.id].room].players = rooms[online[socket.id].room].players.filter(
-      //     (player) => player != socket.id
-      //   );
-
-      // if(rooms[online[socket.id].room].players.length == 0) { // 모두가 방을 비우면 해당 방 삭제
-      //   delete rooms[online[socket.id].room];
-      // }
-
-      // delete online[socket.id]; // 일단 온라인에서 유저도 삭제
+      // 게임 종료 시 다른 사람들한테 전달
+      socket.broadcast.to(socket.roomId).emit('disconnect player', socket.id);
     });
   });
 
