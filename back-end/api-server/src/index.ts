da5b1e9d171b7f042168925267c099142f3fcca2
chanho <chhqm2759@naver.com>
2021-11-16 15:03:21 +0900

    ✨ : swagger 초기 세팅,  API 명세 작업 진행중

--- back-end/api-server/src/index.ts
+++ back-end/api-server/src/index.ts
@@ -12,6 +12,9 @@ import ProfileRouter from '../routes/profile';
 import RankingRouter from '../routes/rankingSearch';
 import FriendRouter from '../routes/friend';
 import GameRecordRouter from '../routes/gameRecord';
+import RankRouter from '../routes/rankSearch';
+import * as swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yamljs';
 
 import { registerDupCheck } from '../middlewares/jwt';
 
@@ -22,6 +25,8 @@ class App {
   }
 }
 const app = new App().application;
+const swaggerSpec = YAML.load(path.join(__dirname, '../build/swagger.yaml'));
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(
@@ -37,8 +42,12 @@ app.use('/api/auth', AuthRouter);
 app.use('/api/rank', RankingRouter);
 app.use('/api/register', registerDupCheck, InsertDbRegister);
 app.use('/api/profile', ProfileRouter);
+<<<<<<< HEAD
 app.use('/api/friend', FriendRouter);
 app.use('/api/game/record', GameRecordRouter);
+=======
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+>>>>>>> 82c3c03 (✨ : swagger 초기 세팅,  API 명세 작업 진행중)
 
 app.get('/api', (req: express.Request, res: express.Response) => {
   res.send('start');
