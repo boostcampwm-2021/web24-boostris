a1b4fa16fb6e44a237c9eb5e7847ab2ae693e774
jeongbbn <kkaatteelyn@gmail.com>
2021-11-18 11:21:38 +0900

    🐛 : DB 칼럼 변경에 따른 profile API 쿼리 수정

--- back-end/api-server/routes/profile.ts
+++ back-end/api-server/routes/profile.ts
@@ -21,8 +21,8 @@ ProfileRouter.post('/stateMessage', async (req, res, next) => {
 
 ProfileRouter.post('/total', async (req, res, next) => {
   try {
-    const totalList = await getTotalInDB(req.body.nickname);
-    const recentList = await getRecentInDB(req.body.nickname);
+    const totalList = await getTotalInDB(req.body.id);
+    const recentList = await getRecentInDB(req.body.id);
     const [total, win] = totalList;
     res.status(200).json({ total, win, recentList });
   } catch (error) {
@@ -59,30 +59,30 @@ const getStateMessageInDB = (nickname) => {
   return selectTable('state_message', 'USER_INFO', `nickname='${nickname}'`);
 };
 
-const getTotalInDB = async (nickname) => {
+const getTotalInDB = async (id) => {
   return await Promise.all([
     selectTable(
-      'SUM(attack_cnt) as total_attack_cnt, COUNT(nickname) as total_game_cnt, SEC_TO_TIME(SUM(TIME_TO_SEC(play_time))) as total_play_time ',
+      'SUM(attack_cnt) as total_attack_cnt, COUNT(oauth_id) as total_game_cnt, SEC_TO_TIME(SUM(TIME_TO_SEC(play_time))) as total_play_time ',
       'PLAY',
-      `nickname='${nickname}'`
+      `oauth_id='${id}'`
     ),
     innerJoinTable(
       `SUM(case when game_mode='1 vs 1' then player_win else 0 end) as single_player_win, SUM(case when game_mode='normal' then player_win else 0 end) as multi_player_win`,
       'PLAY',
       'GAME_INFO',
       'PLAY.game_id = GAME_INFO.game_id',
-      `nickname='${nickname}'`
+      `oauth_id='${id}'`
     ),
   ]);
 };
 
-const getRecentInDB = (nickname) => {
+const getRecentInDB = (id) => {
   return innerJoinTable(
     'game_date, game_mode, ranking, play_time, attack_cnt, attacked_cnt',
     'PLAY',
     'GAME_INFO',
     'PLAY.game_id = GAME_INFO.game_id',
-    `nickname='${nickname}'`
+    `oauth_id='${id}'`
   );
 };
 
